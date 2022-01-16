f510ba8ca43d7b6ab7253668b2c8ecb5d1a8a966
BlackMATov <blackmat@mail.ru>
2022-01-16 15:10:22 +0700

    add classes example

--- manuals/meta_examples/classes_example.cpp
+++ manuals/meta_examples/classes_example.cpp
@@ -8,8 +8,71 @@
 
 namespace
 {
+    class shape {
+    public:
+        virtual ~shape() = default;
+        virtual int get_area() const = 0;
+    };
+
+    class rectangle : public shape {
+    public:
+        explicit rectangle(int width, int height)
+        : width_{width}
+        , height_{height} {}
+
+        int get_width() const {
+            return width_;
+        }
+
+        int get_height() const {
+            return height_;
+        }
+
+        int get_area() const override {
+            return width_ * height_;
+        }
+    private:
+        int width_{};
+        int height_{};
+    };
 }
 
-TEST_CASE("meta/meta_examples/classes") {
+TEST_CASE("meta/meta_examples/classes/type") {
     namespace meta = meta_hpp;
+
+    // 'shape' class type registration
+    meta::class_<shape>()
+        .method_("get_area", &shape::get_area);
+
+    // 'rectangle' class type registration
+    meta::class_<rectangle>()
+        .base_<shape>()
+        .ctor_<int, int>()
+        .method_("get_width", &rectangle::get_width)
+        .method_("get_height", &rectangle::get_height);
+
+    // resolves a class type by static class type
+    const meta::class_type rectangle_type = meta::resolve_type<rectangle>();
+
+    // prints all class methods
+    fmt::print("* rectangle:\n");
+    for ( auto&& [index, method] : rectangle_type.get_methods() ) {
+        fmt::print("  + {}/{}\n", index.name, index.type.get_arity());
+    }
+}
+
+TEST_CASE("meta/meta_examples/classes/usage") {
+    namespace meta = meta_hpp;
+
+    // resolves a class type by static class type
+    const meta::class_type rectangle_type = meta::resolve_type<rectangle>();
+
+    // finds the method 'get_area' by name
+    const meta::method rectangle_area = rectangle_type.get_method("get_area");
+
+    // creates a rectangle instance by the registered constructor(int, int)
+    const meta::value rectangle_v = rectangle_type.create(10, 20).value();
+
+    // calls the method with the dynamic rectangle instance 'rectangle_v'
+    CHECK(rectangle_area.invoke(rectangle_v) == 200);
 }
