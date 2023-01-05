6922c2019bec6556f31655c5742c78a8e214292e
BlackMATov <blackmat@mail.ru>
2023-01-05 08:24:08 +0700

    Happy New Year! 🥳

--- untests/meta_issues/github_issue_15.cpp
+++ untests/meta_issues/github_issue_15.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of the "https://github.com/blackmatov/meta.hpp"
  * For conditions of distribution and use, see copyright notice in LICENSE.md
- * Copyright (C) 2021-2022, by Matvey Cherevko (blackmatov@gmail.com)
+ * Copyright (C) 2021-2023, by Matvey Cherevko (blackmatov@gmail.com)
  ******************************************************************************/
 
 #include "../meta_untests.hpp"
@@ -26,7 +26,7 @@ TEST_CASE("meta/meta_issues/15") {
     meta::class_<rect>()
         .method_("get_width", &rect::get_width)
         .function_("get_width_static", &rect::get_width_static);
-    
+
     int v{};
     rect r{};
 
