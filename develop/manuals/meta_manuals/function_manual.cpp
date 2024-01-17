0356045a12346cc6370a9195d68e469c0d0f2c9e
BlackMATov <blackmat@mail.ru>
2024-01-18 01:44:14 +0700

    Happy New Year! 🥳

--- develop/manuals/meta_manuals/function_manual.cpp
+++ develop/manuals/meta_manuals/function_manual.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of the "https://github.com/blackmatov/meta.hpp"
  * For conditions of distribution and use, see copyright notice in LICENSE.md
- * Copyright (C) 2021-2023, by Matvey Cherevko (blackmatov@gmail.com)
+ * Copyright (C) 2021-2024, by Matvey Cherevko (blackmatov@gmail.com)
  ******************************************************************************/
 
 #include <meta.hpp/meta_all.hpp>
