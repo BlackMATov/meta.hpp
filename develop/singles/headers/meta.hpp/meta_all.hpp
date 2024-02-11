a89f27e0520985eae75946a523c939a72b7ecf68
BlackMATov <blackmat@mail.ru>
2024-02-11 08:39:22 +0700

    remove hashed_string class

--- develop/singles/headers/meta.hpp/meta_all.hpp
+++ develop/singles/headers/meta.hpp/meta_all.hpp
@@ -794,65 +794,6 @@ namespace meta_hpp::detail
 
 namespace meta_hpp::detail
 {
-    class hashed_string final {
-    public:
-        hashed_string() = default;
-        ~hashed_string() = default;
-
-        hashed_string(hashed_string&&) = default;
-        hashed_string(const hashed_string&) = default;
-
-        hashed_string& operator=(hashed_string&&) = default;
-        hashed_string& operator=(const hashed_string&) = default;
-
-        constexpr hashed_string(std::string_view str) noexcept
-        : hash_{fnv1a_hash(str.data(), str.size())} {}
-
-        constexpr void swap(hashed_string& other) noexcept {
-            std::swap(hash_, other.hash_);
-        }
-
-        [[nodiscard]] constexpr std::size_t get_hash() const noexcept {
-            return hash_;
-        }
-
-        [[nodiscard]] constexpr bool operator==(hashed_string other) const noexcept {
-            return hash_ == other.hash_;
-        }
-
-        [[nodiscard]] constexpr std::strong_ordering operator<=>(hashed_string other) const noexcept {
-            return hash_ <=> other.hash_;
-        }
-
-    private:
-        std::size_t hash_{fnv1a_hash("", 0)};
-    };
-
-    constexpr void swap(hashed_string& l, hashed_string& r) noexcept {
-        l.swap(r);
-    }
-
-    [[nodiscard]] constexpr bool operator==(hashed_string l, std::string_view r) noexcept {
-        return l == hashed_string{r};
-    }
-
-    [[nodiscard]] constexpr std::strong_ordering operator<=>(hashed_string l, std::string_view r) noexcept {
-        return l <=> hashed_string{r};
-    }
-}
-
-namespace std
-{
-    template <>
-    struct hash<meta_hpp::detail::hashed_string> {
-        size_t operator()(meta_hpp::detail::hashed_string hs) const noexcept {
-            return hs.get_hash();
-        }
-    };
-}
-
-namespace meta_hpp::detail
-{
     template < std::size_t SizeBytes = sizeof(std::size_t) >
     struct hash_composer_traits {};
 
@@ -880,7 +821,7 @@ namespace meta_hpp::detail
         }
 
         constexpr hash_composer operator<<(std::string_view value) noexcept {
-            hash = combine(hash, hashed_string{value}.get_hash());
+            hash = combine(hash, fnv1a_hash(value.data(), value.size()));
             return *this;
         }
 
